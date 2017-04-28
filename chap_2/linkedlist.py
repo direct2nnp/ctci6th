58cd64832a08bd03884667683aeee52d21b8d202
Nikunj Patel <direct2nnp@gmail.com>
2017-04-28 16:54:02 -0700

    adding node and list

---
+++ chap_2/linkedlist.py
@@ -0,0 +1,72 @@
+import node
+
+class MyLinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert_top(self, data):
+        new_node = node.Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def insert(self, data):
+        if not self.head:
+            self.insert_top(data)
+        else:
+            current = self.head
+            while current and current.get_next():
+                current = current.get_next()
+
+            new_node = node.Node(data)
+            current.set_next(new_node)
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+
+        return count
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print current.get_data()
+            current = current.get_next()
+
+    def search(self, data):
+        current = self.head
+        found = False
+        while found is False and current:
+            if current.get_data() == data:
+                found = True
+            else:
+                current = current.get_next()
+
+        if current == None:
+            raise ValueError('Data is not in list')
+
+        return current
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        found = False
+        while found is False and current:
+            if current.get_data() == data:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if current == None:
+            raise ValueError('Data is not in list')
+        
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+
+            
